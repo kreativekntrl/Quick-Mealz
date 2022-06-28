861be34b10bbe036c833f2c23814555edeb564ff
Ava <avamsteimle@gmail.com>
2022-06-28 12:21:40 -0700

    signup form

--- quick-mealz/src/App.js
+++ quick-mealz/src/App.js
@@ -1,26 +1,28 @@
 import './App.scss';
+import 'bootstrap';
+import NavBar from './components/NavBar';
+import SignUp from './components/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="page-wrap">
-        <header className="page-header">
-    Header
+<div className="page-wrap">
+  <header className="page-header">
+    QuickMealz
   </header>
   <nav className="page-nav">
-    Nav
+    <NavBar />
   </nav>
   <main className="page-main">
     <article>
-      <details>
-        <summary>Article</summary>
-      </details>
+        <SignUp />
     </article>
   </main>
   <aside className="page-sidebar">
     Aside
   </aside>
   <footer className="page-footer">
-    Footer
+    <Footer />
   </footer>
     </div>
   );
---
+++ quick-mealz/src/components/SignUp.js
@@ -0,0 +1,25 @@
+const SignUp = () => {
+    return (
+<div>
+    <form>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputFirstName">First Name</label>
+            <input type="text" className="form-control" id="exampleInputFirstName" placeholder="Enter first name" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputLastName">Last Name</label>
+            <input type="text" className="form-control" id="exampleInputFirstName" placeholder="Enter last name" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword" placeholder="Enter password" />
+        </div>
+    </form>
+</div>
+    );
+}
+export default SignUp;
