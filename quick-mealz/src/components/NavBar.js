7a8d973bb4a1c11ee5073d733c81fdb1276a8e28
Ava <avamsteimle@gmail.com>
2022-06-28 18:53:42 -0700

    NavBar component started

--- quick-mealz/src/components/NavBar.js
+++ quick-mealz/src/components/NavBar.js
@@ -1,7 +1,11 @@
 const NavBar = () => {
     return (
 <div>
-        
+    <ul className="list-inline">
+        <li className="list-inline-item"><a href="">Gallery</a></li>
+        <li className="list-inline-item"><a href="">Pricing</a></li>
+        <li className="list-inline-item"><a href="">Your Orders</a></li>
+    </ul>    
 </div>
     );
 }
